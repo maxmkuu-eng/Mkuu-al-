ad2d8b398e65f96cfe179b30cf954c8c10d234d5
maxmkuu-eng <maxmkuu@gmail.com>
2026-08-22 18:52:38 +0300

    Fix SMS sender Context import

--- android/app/src/main/java/com/mkuu/ai/SmsSenderPlugin.java
+++ android/app/src/main/java/com/mkuu/ai/SmsSenderPlugin.java
@@ -2,6 +2,7 @@ package com.mkuu.ai;
 
 import android.Manifest;
 import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Build;
