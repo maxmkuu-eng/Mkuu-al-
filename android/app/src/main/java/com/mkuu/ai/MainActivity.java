4a83bb5dd26a395a7bf9ae6bd810b0559331b6af
maxmkuu-eng <maxmkuu@gmail.com>
2026-08-22 15:31:47 +0300

    Register Android Gallery saver plugin

--- android/app/src/main/java/com/mkuu/ai/MainActivity.java
+++ android/app/src/main/java/com/mkuu/ai/MainActivity.java
@@ -2,4 +2,10 @@ package com.mkuu.ai;
 
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(android.os.Bundle savedInstanceState) {
+        registerPlugin(GallerySaverPlugin.class);
+        super.onCreate(savedInstanceState);
+    }
+}
