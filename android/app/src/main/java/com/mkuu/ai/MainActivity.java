a6d6fde5a9952da5721667ab22042e2835c03bde
maxmkuu-eng <maxmkuu@gmail.com>
2026-08-22 17:37:58 +0300

    Register native SMS sender plugin

--- android/app/src/main/java/com/mkuu/ai/MainActivity.java
+++ android/app/src/main/java/com/mkuu/ai/MainActivity.java
@@ -15,6 +15,7 @@ public class MainActivity extends BridgeActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         registerPlugin(GallerySaverPlugin.class);
+        registerPlugin(SmsSenderPlugin.class);
         super.onCreate(savedInstanceState);
         requestSmsPermissions();
     }
